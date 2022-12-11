5a773fc1159536d9b7960e1b93fef18dd2cd6ae0
GerasimovVictor <gerasimov.viktor90@gmail.com>
2022-12-11 10:53:56 +0700

    Задача 21

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿/* Задача 19
 Напишите программу, которая принимает на вход пятизначное число
 и проверяет, является ли оно палиндромом.
-*/
+
 
 Console.Write("Введите любое пятизначное число:");
 int number = Convert.ToInt32(Console.ReadLine());
@@ -24,6 +24,35 @@ if (number < 100000 && number >= 10000)
 
 }
 else Console.WriteLine("Неверный ввод числа. ");
-    
+*/
+
+/*Задача 21
+
+Напишите программу, которая принимает на вход координаты двух точек
+и находит расстояние между ними в 3D пространстве.
+*/
+Console.Write("Введите координату х1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+Console.WriteLine("Расстояние между 2 точками: " + Math.Round(d,2));
+
+
 
 
